530ffb9cdfe499714c7c8a38765b22bd5ccdbd50
Elangovan666 <elangovanpersonalmailid@gmail.com>
2025-03-10 20:05:39 +0530

    max_min.py

---
+++ max_min.py
@@ -0,0 +1,10 @@
+Num1 = int(input("Enter the Num1 Value: "))
+Num2 = int(input("Enter the Num2 Value: "))
+
+if (Num1 > Num2):
+    print("The Number is Maximum")
+elif (Num1 < Num2):
+    print("The Number is Minimum")
+else :
+    Num1 == Num2
+    print("The Number is equal")
